102bdf6b016e89605c495014d72c0d50f1d34475
mikakane <mikakane2@gmail.com>
2016-06-05 22:56:20 +0900

    update

--- backend/bootstrap.php
+++ backend/bootstrap.php
@@ -16,22 +16,8 @@ $app->withFacades();
 
 $app->withEloquent();
 
-//$app->register(\Chatbox\WebHook\WebHookServiceProvider::class);
-
-
 $app->get("/",function(){
-    return "application sample";
-});
-
-$app->post("/",function(WebHookService $webHookService){
-
-    $webHookService->load("");
-
-    return $webHookService->handle();
-
-    return [
-        "status" => "OK"
-    ];
+    return "lumen application";
 });
 
 return $app;
